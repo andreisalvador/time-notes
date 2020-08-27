9e21babb3d77ae85940a178cea164660ccf90c1c
Andrei F. Salvador <andreifs95@gmail.com>
2020-08-27 16:13:07 -0300

    Initial Commit

---
+++ src/TimeNotas.App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TimeNotas.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
