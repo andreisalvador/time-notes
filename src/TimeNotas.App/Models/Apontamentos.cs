67e43b780e9df8c314f27b64b537b176341f0190
Andrei F. Salvador <andreifs95@gmail.com>
2020-08-27 20:04:24 -0300

    CRUD Feio

---
+++ src/TimeNotas.App/Models/Apontamentos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeNotas.App.Models
+{
+    public class Apontamentos
+    {
+        private readonly IEnumerable<Apontamento> _apontamentos;
+
+        public Apontamentos(IEnumerable<Apontamento> apontamentos)
+        {
+            _apontamentos = apontamentos;
+            DatasHoras = new Dictionary<string, IEnumerable<Apontamento>>();
+
+            foreach (var item in _apontamentos.GroupBy(a => $"{a.DataHora.DayOfWeek} {a.DataHora.Date.ToString("dd/MM/yyyy")}"))
+                DatasHoras.Add(item.Key,  item.OrderBy(a => a.DataHora));
+
+        }
+
+        [Display(Name = "Data/Hora apontamentos")]
+        public IDictionary<string, IEnumerable<Apontamento>> DatasHoras { get; private set; }
+    }
+}
