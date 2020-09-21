8684e9c20e4ef6fe143d789d295eb3e480310dd7
Andrei Salvador <andreifs95@gmail.com>
2020-09-21 14:00:45 -0300

    Ajustes

--- src/TimeNotes.Core/Extensions/TimeSpanExtensions.cs
+++ src/TimeNotes.Core/Extensions/TimeSpanExtensions.cs
@@ -5,6 +5,6 @@ namespace TimeNotes.Core.Extensions
     public static class TimeSpanExtensions
     {
         public static string ToFormattedStringTime(this TimeSpan value)
-            => $"{value.Hours}h {value.Minutes}m";
+            => $"{value.Hours + (value.Days * 24)}h {value.Minutes}m";
     }
 }
